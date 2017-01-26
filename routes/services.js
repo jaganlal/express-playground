75fc2f709944097b28f0dec4f88322d7abd0a684
Jaganlal Thoppe <jaganlal@gmail.com>
2017-01-25 19:47:35 -0500

    added 2 service apis to get token and name
    added 2 service apis to get token and name

--- routes/services.js
+++ routes/services.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET name page. */
 router.get('/token', function(req, res, next) {
-  res.cookie('newtoken', 'simple-test-token');
-  res.send({name: 'jaganlal'});
-  // res.end();
+  res.cookie('token', new Date().getTime());
+  res.end();
   // res.cookie('token' , 'cookie_value').send({name: 'jaganlal'});
 });
 
